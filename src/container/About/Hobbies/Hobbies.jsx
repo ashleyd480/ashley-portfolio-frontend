f20f0ed842c86c92d88712410ed5adefeb91f488
ashleyd480 <duanashley@gmail.com>
2024-06-22 09:09:18 -0700

    started rough draft of 'my story' and edited the secondary blurb title css to have it be global css as many component use it

--- src/container/About/Hobbies/Hobbies.jsx
+++ src/container/About/Hobbies/Hobbies.jsx
@@ -1,8 +1,11 @@
 const Hobbies = () => {
 
     return (
-    <>
-      <h2 className="blurb-title">Section Under Construction 😊</h2>
+        <>
+                <h2 className="blurb-title"> Hobbies 🐾</h2>
+            <h2 className="blurb-title">Section Under Construction 😊</h2>
+            <div className="blurb-section">
+                </div>
     </>
     );
   
--- src/container/About/MyStory/MyStory.jsx
+++ src/container/About/MyStory/MyStory.jsx
@@ -1,10 +1,40 @@
-const MyStory = () => {
+import Accordion from "react-bootstrap/Accordion";
 
-    return (
+const MyStory = () => {
+  return (
     <>
       <h2 className="blurb-title">Section Under Construction 😊</h2>
+      <div className="blurb-section">
+
+
+        <Accordion defaultActiveKey="0">
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>The Who</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>The Why</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </div>
     </>
-    );
-  
-}
+  );
+};
 export default MyStory;
