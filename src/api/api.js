286194709cc65f3615d86829f1642ce5555981a4
ashleyd480 <duanashley@gmail.com>
2024-06-26 16:04:03 -0500

    updated url

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 /*these are functions we can then call to help "collapse" the api calls in our components so not too lengthy */
 // const apiUrl = "http://localhost:8080";
-const apiUrl = "https://ashley-portfolio-29f132612fe3.herokuapp.com/";
+const apiUrl = "https://ashley-portfolio-29f132612fe3.herokuapp.com";
 export const fetchData = async (endpoint) => {// get request
   try {
     const response = await fetch(`${apiUrl}/${endpoint}`);
