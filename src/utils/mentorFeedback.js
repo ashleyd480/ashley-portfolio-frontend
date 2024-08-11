4ad0a40470ee0ab65ec194705a82f9ee6d8606c9
ashleyd480 <duanashley@gmail.com>
2024-08-11 11:57:25 -0700

    minor typo in file import fixed

--- src/utils/mentorFeedback.js
+++ src/utils/mentorFeedback.js
@@ -9,6 +9,7 @@ import mentor8 from "../assets/feedback-images/mentor-feedback/mentor-8.png";
 import mentor9 from "../assets/feedback-images/mentor-feedback/mentor-9.png";
 import mentor10 from "../assets/feedback-images/mentor-feedback/mentor-10.png";
 import mentor11 from "../assets/feedback-images/mentor-feedback/mentor-11.png";
+import mentor12 from "../assets/feedback-images/mentor-feedback/mentor-12.png";
 
 export const mentorFeedback = [
   {
